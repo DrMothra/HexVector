e1bd4efa403961cfc0b01d838a97cc99d6c2203c
Tony Glover <tony.glover@nottingham.ac.uk>
2016-01-15 16:00:30 +0000

    More midi tests

--- js/midiTest.js
+++ js/midiTest.js
@@ -4,11 +4,25 @@
 
 $(document).ready(function() {
 
+    var player;
+
+    MIDI.loadPlugin({
+        soundfontUrl: "./soundfont/",
+        onsuccess: function() {
+            /// this is the language we are running in
+            /// this sets up the MIDI.Player and gets things going...
+            player = MIDI.Player;
+            player.timeWarp = 1; // speed the song is played back
+            player.loadFile("./assets/layer1.mid", player.start);
+            //player.loadFile("./assets/layer2.mid", player.start);
+        }
+    });
+
     $('#playMidi').on("click", function() {
-        MIDIjs.play("assets/layer1.mid");
+        //MIDIjs.play("assets/layer1.mid");
     });
 
     $('#stopMidi').on("click", function() {
-       MIDIjs.stop();
+       //MIDIjs.stop();
     });
 });
